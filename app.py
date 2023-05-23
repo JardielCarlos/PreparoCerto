454fef1623719652830def327808628740b779a4
Samira Kaline <samirakaline123@gmail.com>
2023-05-23 16:50:28 -0300

    Corrigindos erros

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from helpers.database import db, migrate
 from resource.usuarios import Usuarios
-from resource.Gestores import Gestores
+from resource.gestores import Gestores
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:senhasecreta@localhost:5432/Pweb2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:124356@localhost:5432/Pweb2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # db = SQLAlchemy(app)
