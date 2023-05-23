b95561e6570c90c0669f6491d590aa4ad0e5df0b
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2023-05-23 07:34:36 -0300

    Add files via upload

--- model/gestor.py
+++ model/gestor.py
@@ -1,10 +1,18 @@
 from flask_restful import fields
 from helpers.database import db
+from model.usuario import Usuario
+from sqlalchemy import ForeignKey
 
 userFields = {}
 
-class Gestor(user.User):
-  __tablename__="gestor"
+class Gestor(Usuario, db.Model):
+  __tablename__ = "tb_gestor"
+  __mapper_args__ = {"polymorphic_identity": "gestor"}
+
+  id = db.Column(ForeignKey("tb_usuario.id"), primary_key=True)
+
+  def __init__(self, nome, email, senha):
+    super().__init__(nome, email, senha)
 
   def __repr__(self):
-    return f'<Gestor {self.nome}>'
+    return f"<Gestor {self.nome}>"
--- model/usuario.py
+++ model/usuario.py
@@ -1,13 +1,19 @@
 from flask_restful import fields
 from helpers.database import db
+from sqlalchemy.types import String
 
 userFields = {}
 
 class Usuario(db.Model):
+  __tablename__ = "tb_usuario"
+  
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String, nullable=False)
   email = db.Column(db.String, nullable=False, unique=True)
   senha = db.Column(db.String, nullable=False)
+
+  tipo_usuario = db.Column("tipo_usuario", String(50))
+  __mapper_args__ = {"polymorphic_on": tipo_usuario}
 
   def __init__(self, nome, email, senha):
     self.nome = nome
@@ -15,4 +21,4 @@ class Usuario(db.Model):
     self.senha = senha
 
   def __repr__(self):
-    return f'<User {self.nome}>'
+    return f"<User {self.nome}>"
