d595d1c4709b6682b8e062b3f31233923fd7250d
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2023-06-09 11:59:27 -0300

    fix

--- model/cardapio.py
+++ model/cardapio.py
@@ -1,9 +1,11 @@
 from flask_restful import fields
 from helpers.database import db
+from model.empresa import empresaFields
 
 cardapioFields = {
   'id': fields.Integer,
-  'nome': fields.String
+  'nome': fields.String,
+  'empresa': fields.Nested(empresaFields)
   }
 
 class Cardapio(db.Model):
@@ -11,9 +13,13 @@ class Cardapio(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String, nullable=False)
+  empresa_id = db.Column(db.Integer ,db.ForeignKey("tb_empresa.id"))
 
-  def __init__(self, nome):
+  empresa = db.relationship("Empresa", uselist=False)
+
+  def __init__(self, nome, empresa):
     self.nome = nome
+    self.empresa = empresa
 
   def __repr__(self):
     return f'<Cardapio {self.nome}>'
