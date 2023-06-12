a915ce0ded0bb25136b7ce73cd63583cd3011ae7
Jardiel <Jardiel.CarlosM@hotmail.com>
2023-06-12 16:55:18 -0300

    Fix Error

--- model/preparacao.py
+++ model/preparacao.py
@@ -16,15 +16,15 @@ class Preparacao(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String, nullable=False)
   empresa_id = db.Column(db.Integer, db.ForeignKey("tb_empresa.id"))
-  modoPreparo_id = db.Column(db.Integer, db.ForeignKey("tb_modoPreparo"))
+  # modoPreparo_id = db.Column(db.Integer, db.ForeignKey("tb_modoPreparo"))
 
   empresa = db.relationship("Empresa", uselist=False)
-  modoPreparo = db.relationship("ModoPreparo", uselist=False)
+  # modoPreparo = db.relationship("ModoPreparo", uselist=False)
 
-  def __init__(self, nome, empresa, modoPreparo):
+  def __init__(self, nome, empresa):
     self.nome = nome
     self.empresa = empresa
-    self.modoPreparo = modoPreparo
+    # self.modoPreparo = modoPreparo
 
   def __repr__(self):
     return f'<Preparacao {self.nome}>'
