1a7a0be3e8ff93a574b8e05ccbe0f9f9917f150e
Jardiel <Jardiel.CarlosM@hotmail.com>
2023-06-16 16:51:25 -0300

    update

--- resources/usuario.py
+++ resources/usuario.py
@@ -3,8 +3,8 @@ from helpers.logger import logger
 
 from model.mensagem import Message, msgError
 
-from model.usuario import Usuario, userFields
+from model.usuario import Usuario, userEmailFields
 
 class Usuarios(Resource):
     def get(self):
-        return marshal(Usuario.query.all(), userFields)
+        return marshal(Usuario.query.all(), userEmailFields)
