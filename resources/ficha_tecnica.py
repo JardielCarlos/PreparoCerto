efa515a485f300137d481623c018e91e83b571b4
Jardiel <Jardiel.CarlosM@hotmail.com>
2023-06-20 18:32:23 -0300

    Update

--- resources/ficha_tecnica.py
+++ resources/ficha_tecnica.py
@@ -1,36 +1,45 @@
 from flask_restful import Resource, marshal
 
-from model.ficha_tecnica import fichaTecnicaOperacionalFields, fichaTecnicaGerencialTotalFields
+from model.ficha_tecnica import fichaTecnicaGerencialFields, fichaTecnicaOperacionalFields
 from model.preparacao_ingrediente import PreparacaoIngrediente
 from model.modo_preparo import ModoPreparo
 from model.utensilio_preparacao import UtensilioPreparacao
 from model.mensagem import Message, msgFields
-from sqlalchemy.sql.functions import sum
 
 class FichaTecnicaOperacional(Resource):
   def get(self, id):
     preparacaoIngrediente = PreparacaoIngrediente.query.filter_by(preparacao_id=id).all()
-    return marshal(preparacaoIngrediente, fichaTecnicaOperacionalFields), 200
 
-
-class FichaTecnicaGerencial(Resource):
-  def get(self, id):
-    preparacaoIngrediente = PreparacaoIngrediente.query.filter_by(preparacao_id=id).all()
     modoPreparo = ModoPreparo.query.filter_by(preparacao_id=id).all()
     preparacaoUtensilio = UtensilioPreparacao.query.filter_by(preparacao_id=id).all()
-    print(preparacaoUtensilio)
 
-    if preparacaoIngrediente is None:
-      codigo = Message(1, "A preparação não possui ingredientes")
-      return marshal(codigo, msgFields)
-
-    elif modoPreparo is None:
+    if modoPreparo is None:
       codigo = Message(1, "A preparação não possui um modo de preparo")
       return marshal(codigo, msgFields)
 
     elif preparacaoUtensilio == []:
       codigo = Message(1, "A preparação não possui utensilios")
       return marshal(codigo, msgFields), 404
+
+    data = {
+      "preparacao_ingrediente": preparacaoIngrediente,
+      "modoPreparo": modoPreparo,
+      "preparacao_utensilio": preparacaoUtensilio
+    }
+
+    return marshal(data, fichaTecnicaOperacionalFields), 200
+
+
+class FichaTecnicaGerencial(Resource):
+  def get(self, id):
+    preparacaoIngrediente = PreparacaoIngrediente.query.filter_by(preparacao_id=id).all()
+
+
+    if preparacaoIngrediente is None:
+      codigo = Message(1, "A preparação não possui ingredientes")
+      return marshal(codigo, msgFields)
+
+
 
     valorSugerido = None
     total = 0
@@ -39,10 +48,8 @@ class FichaTecnicaGerencial(Resource):
 
     data = {
       "preparacao_ingrediente": preparacaoIngrediente,
-      "preparacao_utensilio": preparacaoUtensilio,
-      "modoPreparo": modoPreparo,
       "total": total,
       "valor_sugerido": valorSugerido
     }
 
-    return marshal(data, fichaTecnicaGerencialTotalFields), 200
+    return marshal(data, fichaTecnicaGerencialFields), 200
